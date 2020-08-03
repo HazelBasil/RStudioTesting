3eb7f88a677221dbf90db1bba0a0d95f0314f3e2
HazelBasil <k.hazel.kwon@gmail.com>
2020-08-03 14:48:00 -0700

    Final conclusion about using PAT instead of password (due to 2FA) so far.

--- Test1.R
+++ Test1.R
@@ -1,4 +1,4 @@
 print("test")
 print("my token : cf821bfae425ac317b4d212ca636ed3a8c87de57")
 print("test3: after turned it off and restarted")
-
+print("CONCLUSION: YOU NEED TO CREATE PERSONAL ACCESS TOKEN everytime you reopen the project. It does not remmeber PAT")
